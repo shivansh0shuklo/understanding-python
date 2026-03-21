02dec6fd96c2dfd27a29bcf770b2db5811b98d87
Aftermeth <shivanshshukla2080@gmail.com>
2026-03-21 23:56:36 +0530

    ok

--- MAIN PROJECT/MAIN_PROJECT1.py
+++ MAIN PROJECT/MAIN_PROJECT1.py
@@ -1,9 +1,11 @@
+import hashlib
 class FileGuard:
     def __init__(self,directory,baseline):
         self.directory = directory##location  of the file assingned to watched
         self.baseline  = baseline##stores the memory of file 
-    def calculate_hash():
+    def calculate_hash(self):
         pass
+        
 
     def monitor():
         pass
